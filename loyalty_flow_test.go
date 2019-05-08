f51c9047040c266948ae81e724b44b372db33244
shubham-kumar-montee <suvamsinghania@gmail.com>
2019-05-08 16:24:03 +0530

    Latest

--- loyalty_flow_test.go
+++ loyalty_flow_test.go
@@ -9,7 +9,7 @@ import (
 func  TestCheck(t *testing.T) {
 	fmt.Println("Entering TestInit")
 	name:=maincheck()
-        if(name!="shubham"){
+        if(name!="shubham1"){
      t.Errorf("returned value not the same")
 }
 	
